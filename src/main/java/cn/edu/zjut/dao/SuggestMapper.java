ed8668de4d883784496be6f38ace9a662e4455a5
luyiming <740674785@qq.com>
2018-12-29 15:25:04 +0800

    完成了dao层的内容

--- src/main/java/cn/edu/zjut/dao/SuggestMapper.java
+++ src/main/java/cn/edu/zjut/dao/SuggestMapper.java
@@ -1,10 +1,15 @@
 package cn.edu.zjut.dao;
 
+import cn.edu.zjut.po.Suggest;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
 public interface SuggestMapper {
 
-    public boolean listStudentSuggest();
+    public List<Suggest> listStudentSuggest();
 
-    public boolean listTeacherSuggest();
+    public List<Suggest> listTeacherSuggest();
 
-    public int insertTeacherSuggest();
+    public int insertTeacherSuggest(@Param("teacherID") Integer teacherID,@Param("suggest") String suggest, @Param("mtime")String time);
 }
